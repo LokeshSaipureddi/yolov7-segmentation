251e3de289518ce2f92905e1236134aea162bccd
Lokesh Saipureddi <saipureddilokesh@gmail.com>
2023-02-20 10:28:31 +0530

    Update predict.py

--- segment/predict.py
+++ segment/predict.py
@@ -199,7 +199,7 @@ def run(
 
             # Stream results
             im0 = annotator.result()
-            cv2.putText(im0, f'Count {int(n)} ', (0,255,0), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 128, 229), 2)
+            cv2.putText(im0, f'Count {int(n)} ', (0,50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 128, 229), 2)
             if view_img:
                 if platform.system() == 'Linux' and p not in windows:
                     windows.append(p)
